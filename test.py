9aaddf5646d2303b901e21157770659846366a7f
Xavier Romero <xavier@polygon.technology>
2024-09-02 14:40:31 +0200

    5b3456

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from web3 import Web3
 from tx import send_transaction
 
 
-node_url = 'http://127.0.0.1:33004'
+node_url = 'http://34.175.214.161:18123'
 private_key = \
-    '0xcf11e5ea081b71d6493d8f08d0b81c03b64755a29f9f94dc542156a86880545e'
+    '0xe861f24697bdc8f299060864eeec0ab1d400bca4d436ce44c9acf1b68fa79f39'
 w = Web3(Web3.HTTPProvider(node_url))
 account = w.eth.account.from_key(str(private_key))
 
